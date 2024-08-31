2d96d78fd687f91fe618aac68e56dcb88a3f71e6
Tim Irmler <zwazel@hotmail.com>
2024-08-31 16:10:40 +0200

    replicating head?

--- src/lightyear/my_server/mod.rs
+++ src/lightyear/my_server/mod.rs
@@ -10,7 +10,10 @@ use server::{
 use super::{
     lib::SERVER_ADDR,
     my_shared::{
-        lib::{PhysicalPlayerBodyMarker, PLAYER_REPLICATION_GROUP, SERVER_REPLICATION_INTERVAL},
+        lib::{
+            PhysicalPlayerBodyMarker, PhysicalPlayerHeadMarker, PLAYER_REPLICATION_GROUP,
+            SERVER_REPLICATION_INTERVAL,
+        },
         shared_config,
     },
 };
@@ -34,9 +37,55 @@ impl Plugin for MyServerPlugin {
 // Replicate the pre-predicted entities back to the client(s)
 fn replicate_players(
     mut commands: Commands,
-    query_body: Query<(Entity, &Replicated), (Added<Replicated>, With<PhysicalPlayerBodyMarker>)>,
+    query_body: Query<
+        (Entity, &Replicated),
+        (
+            Added<Replicated>,
+            With<PhysicalPlayerBodyMarker>,
+            Without<PhysicalPlayerHeadMarker>,
+        ),
+    >,
+    query_head: Query<
+        (Entity, &Replicated),
+        (
+            Added<Replicated>,
+            With<PhysicalPlayerHeadMarker>,
+            Without<PhysicalPlayerBodyMarker>,
+        ),
+    >,
 ) {
     for (entity, replicated) in query_body.iter() {
+        let client_id = replicated.client_id();
+
+        // for all player entities we have received, add a Replicate component so that we can start replicating it
+        // to other clients
+        if let Some(mut e) = commands.get_entity(entity) {
+            // we want to replicate back to the original client, since they are using a pre-predicted entity
+            let mut sync_target = SyncTarget::default();
+            sync_target.prediction = NetworkTarget::All;
+
+            let replicate = Replicate {
+                sync: sync_target,
+                controlled_by: ControlledBy {
+                    target: NetworkTarget::Single(client_id),
+                    ..default()
+                },
+                // make sure that all entities that are predicted are part of the same replication group
+                group: PLAYER_REPLICATION_GROUP,
+                ..default()
+            };
+            e.insert((
+                replicate,
+                // if we receive a pre-predicted entity, only send the prepredicted component back
+                // to the original client
+                OverrideTargetComponent::<PrePredicted>::new(NetworkTarget::Single(client_id)),
+                // not all physics components are replicated over the network, so add them on the server as well
+                //PhysicsBundle::player(), // we always run in host server, the client will insert this himself
+            ));
+        }
+    }
+
+    for (entity, replicated) in query_head.iter() {
         let client_id = replicated.client_id();
 
         // for all player entities we have received, add a Replicate component so that we can start replicating it
